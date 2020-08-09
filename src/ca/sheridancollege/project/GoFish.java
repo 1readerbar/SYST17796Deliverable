38d1fb8c5d0f2e6755dbf54296626a2804ba1c4c
Barrett <>
2020-08-08 22:54:43 -0400

    commit 1: added all classes and methods

---
+++ src/ca/sheridancollege/project/GoFish.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author The Master
+ */
+public class GoFish {
+    
+    public GoFish(Player player, Deck deck){}
+    
+    public boolean askPlayerForCard(Player player, int card){
+        
+    }
+    
+    public void goFishing(Player player, Hand hand, Deck deck){
+    
+    }
+    
+    public int scores(){
+        return score;
+    }
+    public void endGame(){
+    
+    }
+}
